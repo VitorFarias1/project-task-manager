560cd19a8e1f69eb012f68be22c489c3050d7a8d
vitorfarias1 <vitor.rodriguesfarias@gmail.com>
2025-06-24 22:55:46 -0300

    wip: progess in controller

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Dtos;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,28 +7,29 @@ namespace Controllers
     [Microsoft.AspNetCore.Components.Route("api/[controller]")]
     public class ApiController : ControllerBase
     {
+        
         [HttpGet("/task")]
         public IActionResult GetTasks()
         {
-
+            return Ok();
         }
 
         [HttpGet("/task/{id}")]
         public IActionResult GetTaskById(Guid id)
         {
-
+            return Ok();
         }
 
         [HttpPost("/task")]
         public IActionResult CreateTask([FromBody] TaskDto task)
         {
-
+            return Created();
         }
 
         [HttpPut("/task/{id}")]
         public IActionResult UpdateTask(Guid id)
         {
-
+            
         }
 
         [HttpDelete("/task/{id}")]
